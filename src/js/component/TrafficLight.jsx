85fd7f6dfb1ce2b066cae2a8ef9b23ec8e525a07
Eric Castillo <erictomas27@hotmail.com>
2024-10-22 05:06:32 +0000

    update

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from "react";
+
+const TrafficLight = () => {
+	const [color, setActiveColor] = useState(null);
+	const colors = ["red", "yellow", "green"]
+
+	const handleClick = (color)=>{
+		setActiveColor(color);
+	}
+
+	const handleLightCycle = ()=>{
+		let currentIndex = colors.indexOf(color);
+		currentIndex = currentIndex ? currentIndex : -1
+		const nextColor = (currentIndex + 1) % colors.length
+		setActiveColor(colors[nextColor])
+	}
+
+	useEffect(()=>{
+		let interval;
+		if(color){
+			interval = setInterval(handleLightCycle, 1000);
+			return () => clearInterval(interval);
+		}
+	}, [color])
+
+	return (
+		<div className="trafficSquare">
+			<div className="littleSquare"></div>
+			<div className="square">
+				<div className={`circle red ${color === "red" ? "active" : ""}`} style={{backgroundColor: "red"}} onClick={()=>{handleClick("red")}}></div>
+				<div className={`circle yellow ${color === "yellow" ? "active" : ""}`} style={{backgroundColor: "yellow"}} onClick={()=>{handleClick("yellow")}}></div>
+				<div className={`circle green ${color === "green" ? "active" : ""}`} style={{backgroundColor: "green"}} onClick={()=>{handleClick("green")}}></div>
+			</div>
+			<button onClick={handleLightCycle}>Cycle light</button>
+		</div>
+	);
+};
+
+export default TrafficLight;
