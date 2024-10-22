b90c706cfa162c2ad50edab4fb08b3fa8fded0d3
Eric Castillo <erictomas27@hotmail.com>
2024-10-22 18:09:25 +0000

    finished

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,38 +1,57 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState, setState } from "react";
 
 const TrafficLight = () => {
 	const [color, setActiveColor] = useState(null);
-	const colors = ["red", "yellow", "green"]
-	let currentIndex;
+	const [buttonClicked, setButtonClicked] = useState(false);
+	const [showPurple, setShowPurple] = useState(false);
+	const [isCycling, setCycling] = useState(false);
+	const [colors, setColors] = useState(["red", "yellow", "green"])
 
-	const handleClick = (color)=>{
-		setActiveColor(color);
-	}
 
-	const handleLightCycle = ()=>{
-		currentIndex = colors.indexOf(color);
-		const nextColor = (currentIndex + 1) % colors.length
-		setActiveColor(colors[nextColor])
-	}
-
-	useEffect(()=>{
-		let interval;
-		if(color){
-			interval = setInterval(handleLightCycle, 1000);
-			return () => clearInterval(interval);
+	const updateStyles = () => {
+		if (!colors.includes("purple")) {
+			setShowPurple(true);
+			setColors(prevColors => [...colors, "purple"])
 		}
-		
-	}, [color])
+
+	}
+
+	const handleClick = (color) => {
+		if (isCycling) setCycling(false), setButtonClicked(false)
+		setActiveColor(color);
+		setButtonClicked(false);
+	}
+
+	const handleLightCycle = () => {
+		const nextColor = (colors.indexOf(color) + 1) % colors.length
+		setActiveColor(colors[nextColor])
+		setButtonClicked(true);
+		setCycling(true);
+	}
+
+	useEffect(() => {
+		let interval;
+		if (color && buttonClicked && isCycling) {
+			interval = setInterval(handleLightCycle, 1000);
+		}
+		return () => clearInterval(interval);
+
+	}, [color, isCycling])
 
 	return (
 		<div className="trafficSquare">
 			<div className="littleSquare"></div>
-			<div className="square">
-				<div className={`circle red ${color === "red" ? "active" : ""}`} style={{backgroundColor: "red"}} onClick={()=>{handleClick("red")}}></div>
-				<div className={`circle yellow ${color === "yellow" ? "active" : ""}`} style={{backgroundColor: "yellow"}} onClick={()=>{handleClick("yellow")}}></div>
-				<div className={`circle green ${color === "green" ? "active" : ""}`} style={{backgroundColor: "green"}} onClick={()=>{handleClick("green")}}></div>
+			<div className="square" style={{ height: showPurple ? "370px" : "280px" }}>
+				<div className={`circles red ${color === "red" ? "active" : ""}`} style={{ backgroundColor: "red" }} onClick={() => { handleClick("red") }}></div>
+				<div className={`circles yellow ${color === "yellow" ? "active" : ""}`} style={{ backgroundColor: "yellow" }} onClick={() => { handleClick("yellow") }}></div>
+				<div className={`circles green ${color === "green" ? "active" : ""}`} style={{ backgroundColor: "green" }} onClick={() => { handleClick("green") }}></div>
+				{showPurple && <div className={`circles purple ${color === "purple" ? "active" : ""}`} style={{ backgroundColor: "purple" }} onClick={() => { handleClick("purple") }}></div>}
 			</div>
-			<button onClick={handleLightCycle}>Cycle light</button>
+			<div className="buttons">
+				<button onClick={handleLightCycle}>Cycle light</button>
+				<button onClick={updateStyles}>Add circle purple</button>
+			</div>
+
 		</div>
 	);
 };
